b12024ef746449a022636f97b53797d0bee0fb17
Ryan Peden <ryan@rpeden.com>
2022-08-31 12:14:47 -0400

    Update flow.py
    Fixed spacing in flow.py

--- flows/flow.py
+++ flows/flow.py
@@ -4,18 +4,15 @@ from prefect import flow, task
 def say_hello(name):
     print(f"hello {name}")
 
-
 @task
 def say_goodbye(name):
     print(f"goodbye {name}")
 
-
 @flow(name="test flow")
-def greetings(names):
+def greetings(names=["arthur", "trillian", "ford", "marvin"]):
     for name in names:
         say_hello(name)
         say_goodbye(name)
-
 
 if __name__ == "__main__":
     greetings(["arthur", "trillian", "ford", "marvin"])
